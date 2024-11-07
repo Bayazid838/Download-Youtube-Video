243f51dcde3c6fccd9ed42359e076d6b4068b69d
LifeLesS_BayaZiD838 <122613177+Bayazid838@users.noreply.github.com>
2024-11-07 19:44:29 +0600

    Add files via upload

--- youtube_mp4_converter.py
+++ youtube_mp4_converter.py
@@ -1,6 +1,6 @@
 from pytube import YouTube  
 from tkinter import filedialog
-import tkinter as 
+import tkinter as tk 
 
 def download_video(url, save_path):
     try:
@@ -18,15 +18,15 @@ def open_file_dialog():
         print("Selected folder:", folder)
         return folder
 
-window = tkinter.Tk()
+window = tk.Tk()  
 window.withdraw()
 
 if __name__ == "__main__":
-    video_url = input("Please enter a YouTube url: ")
+    video_url = input("Please enter a YouTube URL: ")
     save_dir = open_file_dialog()
 
     if save_dir:
         print("Download Started...")
         download_video(video_url, save_dir)
     else:
-        print("Invalid video url!")
+        print("Invalid video URL!")
